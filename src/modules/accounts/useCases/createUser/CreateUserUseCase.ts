236ba07a7171ca97705688c3ac6e436f68c932ba
PauloRog <paulo-rpf@hotmail.com>
2021-10-06 21:43:05 -0300

    Creating password hash

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,8 +1,10 @@
-import { inject } from 'tsyringe';
+import { hash } from 'bcrypt';
+import { inject, injectable } from 'tsyringe';
 
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 
+@injectable()
 class CreateUserUseCase {
   constructor(
     @inject('UsersRepository')
@@ -16,11 +18,18 @@ class CreateUserUseCase {
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists!');
+    }
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
       username,
       email,
-      password,
+      password: passwordHash,
       driver_license,
     });
   }
