a96a37ac10d95c8c6d8dbe9d9404f37ebaa96aea
PauloRog <paulo-rpf@hotmail.com>
2021-10-18 21:29:02 -0300

    Creating rental listings by user

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -8,7 +8,7 @@ import { AppError } from '@shared/errors/AppError';
 
 interface IRequest {
   id: string;
-  user_id: string;
+  // user_id: string;
 }
 
 @injectable()
@@ -22,7 +22,7 @@ class DevolutionRentalUseCase {
     private dateProvider: IDateProvider,
   ) {}
 
-  async execute({ id, user_id }: IRequest): Promise<Rental> {
+  async execute({ id }: IRequest): Promise<Rental> {
     const minimum_daily = 1;
     let total = 0;
     const rental = await this.rentalsRepository.findById(id);
